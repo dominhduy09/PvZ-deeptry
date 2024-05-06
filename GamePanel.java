953e1a22f88bfa5e1702ef69367968993bfcdd1e
Ze1Z1 <162959395+Ze1Z1@users.noreply.github.com>
2024-05-06 15:20:27 +0700

    fix

--- GamePanel.java
+++ GamePanel.java
@@ -1,6 +1,13 @@
 import java.awt.Container;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
-public class GamePanel {
+import javax.swing.JLayeredPane;
+
+public class GamePanel extends JLayeredPane implements MouseMotionListener {
+    private Image sunflowerImage;
+    private Image sunImage;
 
     public void remove(Sun sun) {
         // TODO Auto-generated method stub
@@ -10,6 +17,18 @@ public class GamePanel {
     public Container getActiveSuns() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getActiveSuns'");
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseDragged'");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseMoved'");
     }
 
 }
