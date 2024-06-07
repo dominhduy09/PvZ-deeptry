f74abafad8bd4fdb509ce51ee3d1a2d1327f0867
Ze1Z1 <162959395+Ze1Z1@users.noreply.github.com>
2024-06-07 21:42:20 +0700

    commit

--- Flag.java
+++ Flag.java
@@ -1,19 +1,17 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Flag here.
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Flag extends Actor
-{
+public class Flag extends Actor {
     /**
      * Act - do whatever the Flag wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
+    public void act() {
         // Add your action code here.
     }
 }
